0c7bfc1ca81d97ce32c9fa3dab676f111ed27dfc
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-29 20:15:11 +0500

    completed

---
+++ app.js
@@ -0,0 +1,125 @@
+// function hello() {
+//   var name = "Farooq";
+//   console.log(name);
+// }
+
+// hello();
+
+// function greet2(name) {
+//   var stauts = "Farooq";
+//   if (stauts === name) {
+//     console.log("Ok!");
+//   } else {
+//     console.log("Not Ok!");
+//   }
+//   return status;
+// }
+// console.log(greet2("Farooq"));
+
+//global
+// if (true) {
+//   var name = "Farooq";
+//   console.log(name);
+// }
+// console.log(name);
+
+var globalVar = "I am global";
+
+// function myFunction() {
+//   console.log(globalVar);
+// }
+
+// myFunction(); // Output: "I am global"
+
+// function myFunction() {
+//   var localVar = "I am local";
+//   console.log(localVar); // Accessible here
+// }
+
+// myFunction();
+// console.log(localVar);
+
+// var a = "farooq";
+
+// console.log(a);
+
+// var dayOfWk = prompt("Please enter number");
+
+// switch (dayOfWk) {
+//   case "Sat":
+//     console.log("Saturday!");
+//     break;
+//   case "Mon":
+//     console.log("Monday!");
+//   default:
+//     console.log("Any!");
+// }
+
+// var index = 0;
+// while (index < 3) {
+//   console.log(index);
+//   index++;
+// }
+
+// var j = 4;
+// do {
+//   console.log("sdsafjnsfhhfsd");
+//   j++;
+// } while (j < 3);
+
+// for (var j = 4; j < 3; j++) {
+//   console.log("sfhjsdfgusbf");
+// }
+
+/*
+var j = 4;
+while (j < 3) {
+  console.log("Sdsadsa");
+  j++;
+}
+*/
+
+// var today = new Date ();
+// console.log(today);
+
+// var  doomsday = new Date ("jun 30, 2035")
+// console.log(doomsday.setDate(1));
+
+// function str() {
+//   var str = "sara";
+//   console.log(str);
+//   return str;
+// }
+// str();
+// str();
+// str();
+
+// function str (a){
+//       console.log(a + " " +"by");
+//       return a
+// }
+// str ("hy")
+
+// var str = prompt("Enter your name")
+// switch (str) {
+//     case  "sara":
+//         alert("yes")
+//         break;
+// case "saima":
+//     alert("good")
+//     break;
+//     default:
+//         alert("no name....")
+
+// }
+
+// var i = 3;
+// while(3 < 4){
+// alert("yes..")
+// i++;
+// }
+// var i = 3;
+// do{
+//     alert("yes")
+//     i++;
+// }while(i < 4)
